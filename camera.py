3c2046afc7b89f57263f0ef5e965b1bb7cdd69cb
JosephMusya <josephmusya254@gmail.com>
2022-06-25 15:22:39 +0300

    first commit

--- camera.py
+++ camera.py
@@ -1,4 +1,5 @@
 import cv2
+import sys
 cap = cv2.VideoCapture(0)
 params = cv2.aruco.DetectorParameters_create()
 
@@ -8,4 +9,5 @@ def get_frame():
     if cv2.waitKey(1) & 0xFF == ord('q'):                
         cap.release()
         cv2.destroyAllWindows()
+        sys.exit()
     return frame
