8620a6d06e0ca08ec31f37a04655e99fe8bf0b29
JosephMusya <josephmusya254@gmail.com>
2022-06-25 15:22:22 +0300

    first commit

--- test.py
+++ test.py
@@ -1,23 +1,20 @@
-'''
-Not that this code has been developed for testing purpose only.
-The structure of the robot is defined in the trajectory library
-in kinematics section
-'''
+import cv2
+import camera
+import aruco
+params = cv2.aruco.DetectorParameters_create()
+aruco_dict = cv2.aruco.Dictionary_get(
+    cv2.aruco.DICT_5X5_50)
 
-from trajectory import goTo
+while True:    
+    frame = camera.get_frame()   
+    frame,corners,id = aruco.aruco_marker(frame,aruco_dict,params)
+    try:
+        if len(id)>0:
+            CAM_OFFSET = aruco.distance(corners,frame,id)
+            print(CAM_OFFSET)
+    except TypeError:
+        print("Markers not detected")
 
-#Defining the start and the end position of the robot arm
-HOME_POS = [11,0,2]
-TARGET = [3,7,0]
-t = 5
-H0_6 = [ #Orientation of the end effector
-    [0,1,0,0],
-    [0,0,1,0],
-    [1,0,0,0],
-    [0,0,0,1]
-]
-#Calling the function to execute the motion
-goTo(HOME_POS,
-     TARGET,
-     t,
-     H0_6)
+    frame = cv2.flip(frame,1)             
+
+    cv2.imshow("Aruco Marker",frame)
