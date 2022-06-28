1b44ece7a95a594fb82162216f0bc16d5929a5f6
JosephMusya <josephmusya254@gmail.com>
2022-06-28 23:48:20 +0300

    Edit

--- test.py
+++ test.py
@@ -12,11 +12,11 @@ while True:
     try:
         if len(id) > 0:
             CAM_OFFSET = aruco.distance(corners,frame,id,kwargs=[])            
-            dist = aruco.distance(corners, frame, id, kwargs = [100,150,89])
+            dist = aruco.distance(corners, frame, id, kwargs = [300,300,89])
             # print("Camera offset:  ",CAM_OFFSET)
             print("Distance: ",round(dist[0],2),round(dist[1],2),round(dist[2],2))
     except Exception as e:
         print("Error: ",e)
 
-    cv2.circle(frame,(100,150),4,(0,0,0),-1)
+    cv2.circle(frame,(300,300),4,(0,0,0),-1)
     cv2.imshow("Aruco Marker",frame)
