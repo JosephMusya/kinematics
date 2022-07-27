cd86852b71a2d051277c9f809054d42c96783682
JosephMusya <josephmusya254@gmail.com>
2022-07-27 23:12:24 +0300

    Changed to webcam

--- camera.py
+++ camera.py
@@ -1,13 +1,20 @@
 import cv2
 import sys
-cap = cv2.VideoCapture(0)
-params = cv2.aruco.DetectorParameters_create()
-
-def get_frame():   
+cap = cv2.VideoCapture(-1)
+#cap.set(3,640)
+#cap.set(4,480)
+#cap.set(cv2.CAP_PROP_FRAME_WIDTH,320)
+#cap.set(cv2.CAP_PROP_FRAME_HEIGHT,320)
+def get_frame():
     _,frame = cap.read()
-    
-    if cv2.waitKey(1) & 0xFF == ord('q'):                
+    if cv2.waitKey(1)== ord('q'):
         cap.release()
         cv2.destroyAllWindows()
         sys.exit()
     return frame
+if __name__== '__main__':
+    while True:        
+        frame = get_frame()
+        print(frame)
+        cv2.imshow("CAM TEST",frame)
+    
